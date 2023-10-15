0bce0b7ad28072b41b272de44bc33395f7996a80
Chen1609 <kolelyh@gmail.com>
2023-10-15 10:53:12 +0300

    finally fixed confetti

--- confetti.js
+++ confetti.js
@@ -44,7 +44,7 @@ var removeConfetti; //call to stop the confetti animation and remove all confett
 		if (canvas === null) {
 			canvas = document.createElement("canvas");
 			canvas.setAttribute("id", "confetti-canvas");
-			canvas.setAttribute("style", "display:block;z-index:999999;pointer-events:none");
+			canvas.setAttribute("style", "position:fixed;top:0;right:0;bottom:0;left:0;");
 			document.body.appendChild(canvas);
 			canvas.width = width;
 			canvas.height = height;
